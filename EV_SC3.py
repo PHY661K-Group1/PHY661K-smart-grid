473744bd809d19ab8651a85a0638e4e3fc5f122c
PHY661K-Group1 <48716957+PHY661K-Group1@users.noreply.github.com>
2019-03-19 15:35:10 +0100

    Update EV_SC3.py

--- EV_SC3.py
+++ EV_SC3.py
@@ -72,14 +72,14 @@ if __name__ == '__main__':
     
     update_counts = {}  #for debugging  
     update_counts_sa = {}   #for adaptive learning rate
-    tracker_action = {} #check how many time each state is visited
+    
     
     for k in states:     #{0: {'C': 1.005, 'D': 1.0},1: {'C': 1.005, 'D': 1.0}}
         update_counts_sa[k] = {}
-        tracker_action[k] = {}
+
         for a in ALL_POSSIBLE_ACTIONS:
             update_counts_sa[k][a] = 1.0
-            tracker_action[k][a] = 0
+     
             
     t = 1.0
     deltas = []
@@ -105,17 +105,16 @@ if __name__ == '__main__':
  
             if day == 9999:
                 a, _ = max_dict(Q[s])
+                r = ev.charge_SC3(a, elec_price[s[0]]) 
                 ev.last_policy[(ev.day_time, ev.battery_level)] = a
                 cost_last_day = ev.cost_per_day
                 #print('espilon:',0.9/t)
 
             else: 
                 a = random_action(a, ev.actions[s],eps=0.99)
+                r = ev.charge_SC3(a, elec_price[s[0]]) 
             
-            r = ev.charge_SC3(a, elec_price[s[0]]) 
-            tracker_action[s][a] +=1 
-            #tracker[(ev.day_time, ev.battery_level)] += 1
-            
+     
             s2 = ev.new_current_state()
             
             alpha = ALPHA/update_counts_sa[s][a] 
@@ -126,8 +125,6 @@ if __name__ == '__main__':
             a2, max_q_s2a2 = max_dict(Q[s2])
             Q[s][a] = Q[s][a] + alpha*(r + GAMMA*max_q_s2a2 - Q[s][a])
             biggest_change = max(biggest_change, np.abs(old_qsa - Q[s][a]))
-            
-         
             
             s = s2
             a = a2
